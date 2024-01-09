b32cc860b5dd11d8c220eb21a7fc7386aa903fcc
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2024-01-09 11:40:42 -0300

    Att: Configurações para obter as informações do banco de dados

--- config.php
+++ config.php
@@ -1,1 +1,11 @@
 <?php
+
+return [
+    'db' => [
+        'host' => 'localhost',
+        'daname' => "curso_pdo",
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+    ],
+];
