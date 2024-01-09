d8f2a4aa5b17d4261c18baf2c36acc9ef06b07fb
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2024-01-09 09:13:02 -0300

    Att: Estrutura basica para aprender PDO

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+use app\classes\Uri;
+use app\classes\Routes;
+
+require "../bootstrap.php";
+
+$routes = [
+    '/' => 'controllers/index.php',
+];
+
+$uri = Uri::load();
+
+require Routes::load($routes, $uri);
+
+
