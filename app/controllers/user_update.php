61ce29ab84d8689a2f2ecede79fb552bfc3bc5d4
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2024-03-15 11:54:14 -0300

    add: locate to home

--- app/controllers/user_update.php
+++ app/controllers/user_update.php
@@ -9,4 +9,8 @@ $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
 //Carrega dentro da variavel $user que recebe da tabela de users, para dar um Update utilizando a função update
 $updated = $user->update($_POST,['id' =>$id]);
 
+if($updated) {
+    header('location:/');
+}
+
 dd($updated);
